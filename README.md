a576f9b942b6e5a1240be5ab446c07b02a1a2d08
entbappy <entbappy73@gmail.com>
2024-03-04 11:02:02 +0600

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # End-to-end-Object-Detection-Project
+
+
+## How to run:
+
+```bash
+conda create -n signlang python=3.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'iNeuron',
+    author_email= 'boktiar@ineuron.ai',
+    packages= find_packages(),
+    install_requires = []
+
+)
